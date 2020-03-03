3cd446b91b0a6426710a57b12740b2fd3514e0b6
Rachel Stulgin <rstulgin@gmail.com>
2020-03-03 16:42:50 -0600

    added html, basic css, no js yet

---
+++ main.js
@@ -0,0 +1,12 @@
+/*'use strict';
+
+function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const userName = $('#js-search-term').val();
+      const maxResults = $('#js-max-results').val();
+      getRepos(userName, maxResults);
+    });
+  }
+
+  $(watchForm);*/
