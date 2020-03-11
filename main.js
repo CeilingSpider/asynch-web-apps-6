f161f0ff65f616e110b3d9df4ba033a50dd107cb
Rachel Stulgin <rstulgin@gmail.com>
2020-03-11 15:42:15 -0500

    removed max results as a search parameter

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 /*'use strict';
 
+
+
+
+
+
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
       const userName = $('#js-search-term').val();
-      const maxResults = $('#js-max-results').val();
-      getRepos(userName, maxResults);
+      getRepos(userName);
     });
   }
 
   $(watchForm);*/
+
